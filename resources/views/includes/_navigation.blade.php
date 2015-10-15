e42d01ab1f3c22a29d02f70fa25f207a695b922b
Martin Parsons <Github.MP@the-parsons.me.uk>
2015-10-15 14:46:03 +0100

    updated banner image

--- resources/views/includes/_navigation.blade.php
+++ resources/views/includes/_navigation.blade.php
@@ -1,4 +1,4 @@
-<a href="/"><img src="http://placehold.it/960x200"></a>
+<a href="/"><img src="http://placehold.it/960x220"></a>
 <div class="navbar">
 	<a href = "" class="nav-link">Home</a>
 	<a href="" class="nav-link">About</a>
