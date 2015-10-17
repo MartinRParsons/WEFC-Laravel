a6d162426a706d660bd90c319c0e453b20c566cf
Martin Parsons <Github.MP@the-parsons.me.uk>
2015-10-17 16:30:35 +0100

    First commit to GitHub

--- resources/views/includes/_navigation.blade.php
+++ resources/views/includes/_navigation.blade.php
@@ -1,4 +1,8 @@
-<a href="/"><img src="http://placehold.it/960x220"></a>
+<div class="header-banner">
+	<a href="/"><img class="header-logo" src="/images/WEFClogo.png"></a>
+	<div class="banner-text">Welling Evangelical </br> Free Church</div>
+	<img class="FIEC-logo" src="/images/FIEClogo.png">
+</div>
 <div class="navbar">
 	<a href = "" class="nav-link">Home</a>
 	<a href="" class="nav-link">About</a>
