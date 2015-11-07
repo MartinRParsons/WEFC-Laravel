dee80650037bff11f93a50c04775ac36987f2562
Martin Parsons <Github.MP@the-parsons.me.uk>
2015-11-07 13:15:53 +0000

    Updated Friday Club template

--- resources/views/FridayClub.blade.php
+++ resources/views/FridayClub.blade.php
@@ -1,3 +1,15 @@
+@extends('layouts.master')
+
+@section('title', 'Friday Club')
+
+@section('sidebar')
+    
+@endsection
+
+@section('content')
+
+	
+
 <h1>Friday Club</h1>
 <h2>When?</h2>
 7:30 – 9:00 
@@ -23,3 +35,7 @@ We also have different activities every so often such as debates, creating radio
 Each week a different subject, bible passage or influential person will be presented to the young people with the option for them to ask questions. As a team we aim to choose subjects that will directly apply to their daily lives and will help them in some way. 
 <h2>Why?</h2> 
 We run our Friday club to give young people from the area the opportunity and environment to have fun and socialise outside of school. The bible discussion time gives them the chance to open up and receive to support from young adults on the team who have been through similar challenges, for example, GCSES. 
+
+
+
+@endsection
