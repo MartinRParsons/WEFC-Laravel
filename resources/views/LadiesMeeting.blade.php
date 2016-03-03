94c514870eaec4d84d62ed02d0b0fbaafcfb5d86
Martin Parsons <Github.MP@the-parsons.me.uk>
2016-03-03 16:15:10 +0000

    Added quotation styles

--- resources/views/LadiesMeeting.blade.php
+++ resources/views/LadiesMeeting.blade.php
@@ -7,15 +7,19 @@
 @endsection
 
 @section('content')
-<h1>Ladies Meeting</h1>
-<p>Our meeting is open to ladies of all ages, meeting usually in the church kitchen from 2-3pm each Thursday afternoon during school term time. </p>
-<p>Our aim is to encourage each other, support each other when life gets tough (and it does at times), to share what the Bible teaches in a way that relates to us so we can see how God understands and can give direction and help. </p>
-<p>Prayer is also important to us, as we pray for each other and situations both close and far from home. All are free to pray or join in silently.</p>
-<p>We also like singing and praising God, it lifts up our hearts. You don't have to be good at it, some of us aren't!</p>
-<p>We have a varied programme from our usual Bible study, prayer and praise; to visiting folk who like our company and cheery faces; to having a visiting speaker who might share what they do, for example, a practise nurse speaking about women's health, a lady giving us a cake decorating lesson for Christmas and also someone speaking on fostering children and what that involves. </p>
-<p>Then there are our outings, usually at the end of term but not always. We have been to Ruxley garden centre, Trossley Park and Hall Place, to name but a few.</p>
-<p>Our aim as a group of ladies is to be a blessing to each other.</p>
-<p class='BibleVerse'>Trust in Him at all times; pour out your heart to Him, for God is our refuge.<p>
-<p class='BibleReference'>Psalm 62:8</p>
+<div class="spacer">
+  <h1>Ladies Meeting</h1>
+  <p>Our meeting is open to ladies of all ages, meeting usually in the church kitchen from 2-3pm each Thursday afternoon during school term time. </p>
+  <p>Our aim is to encourage each other, support each other when life gets tough (and it does at times), to share what the Bible teaches in a way that relates to us so we can see how God understands and can give direction and help. </p>
+  <p>Prayer is also important to us, as we pray for each other and situations both close and far from home. All are free to pray or join in silently.</p>
+  <p>We also like singing and praising God, it lifts up our hearts. You don't have to be good at it, some of us aren't!</p>
+  <p>We have a varied programme from our usual Bible study, prayer and praise; to visiting folk who like our company and cheery faces; to having a visiting speaker who might share what they do, for example, a practise nurse speaking about women's health, a lady giving us a cake decorating lesson for Christmas and also someone speaking on fostering children and what that involves. </p>
+  <p>Then there are our outings, usually at the end of term but not always. We have been to Ruxley garden centre, Trossley Park and Hall Place, to name but a few.</p>
+  <p>Our aim as a group of ladies is to be a blessing to each other.</p>
+  <blockquote>
+    <p class='BibleVerse'><q>Trust in Him at all times; pour out your heart to Him, for God is our refuge.</q></p>
+    <p class='BibleReference'>Psalm 62:8</p>
+  </blockquote>
+</div>
 
 @endsection
