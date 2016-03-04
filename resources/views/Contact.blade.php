bc76510a7600fae331e90b3dbf0a75fad977036c
Martin Parsons <Github.MP@the-parsons.me.uk>
2016-03-04 15:43:42 +0000

    Removed Google Map as not working properly on mobile

--- resources/views/Contact.blade.php
+++ resources/views/Contact.blade.php
@@ -22,8 +22,8 @@
       Email - <a href="mailto:info@wefc.church">info@wefc.church</a><br>
 			Phone - 07783 334014
     </p>
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2485.511539058703!2d0.10853801661329762!3d51.467124121466995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8aedbfad486bb%3A0x392527566961d360!2sWelling+Evangelical+Free+Church!5e0!3m2!1sen!2suk!4v1457105077987" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-	</div>
+		<!-- <iframe class="googleMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2485.511539058703!2d0.10853801661329762!3d51.467124121466995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8aedbfad486bb%3A0x392527566961d360!2sWelling+Evangelical+Free+Church!5e0!3m2!1sen!2suk!4v1457105077987" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div> -->
 
 
 @endsection
