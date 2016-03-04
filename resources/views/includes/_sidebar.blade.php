f8c2fa4acde90161ff70b37884e31151b32d6878
Martin Parsons <Github.MP@the-parsons.me.uk>
2016-03-04 14:09:53 +0000

    fixed H2 styling for mobile

--- resources/views/includes/_sidebar.blade.php
+++ resources/views/includes/_sidebar.blade.php
@@ -3,31 +3,31 @@
   <ul>
     <li>
       <h6>Sunday morning service</h5>
-      <p>21st February 11:00</p>
+      <p>6th March 11:00</p>
     </li>
     <li>
       <h6>Sunday evening service</h6>
-      <p>21st February 18:30</p>
+      <p>6th March 18:30</p>
     </li>
     <li>
       <h6>Bible study</h6>
-      <p>23rd February 20:00</p>
+      <p>8th March 20:00</p>
     </li>
     <li>
       <h6>Wednesday club</h6>
-      <p>24th February 18:00</p>
+      <p>9th March 18:00</p>
     </li>
     <li>
       <h6>Coffee morning</h6>
-      <p>25th February 10:30</p>
+      <p>10th March 10:30</p>
     </li>
     <li>
       <h6>Ladies meeting</h6>
-      <p>25th February 14:00</p>
+      <p>10th March 14:00</p>
     </li>
     <li>
       <h6>Friday club</h6>
-      <p>26th February 19:30</p>
+      <p>11th March 19:30</p>
     </li>
   </ul>
 </div>
